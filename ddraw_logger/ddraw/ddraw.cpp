0610f71dd0b8434781952e59ce5de67c82cf5b26
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2024-07-03 03:50:40 +0200

    fix VS build

--- ddraw_logger/ddraw/ddraw.cpp
+++ ddraw_logger/ddraw/ddraw.cpp
@@ -223,3 +223,25 @@ extern "C" HRESULT WINAPI DirectDrawEnumerateW(
 	return hr;
 }
 
+#pragma comment(linker, "/export:AcquireDDThreadLock=C:\\Windows\\System32\\ddraw.AcquireDDThreadLock,@1")
+#pragma comment(linker, "/export:CompleteCreateSysmemSurface=C:\\Windows\\System32\\ddraw.CompleteCreateSysmemSurface,@2")
+#pragma comment(linker, "/export:D3DParseUnknownCommand=C:\\Windows\\System32\\ddraw.D3DParseUnknownCommand,@3")
+#pragma comment(linker, "/export:DDGetAttachedSurfaceLcl=C:\\Windows\\System32\\ddraw.DDGetAttachedSurfaceLcl,@4")
+#pragma comment(linker, "/export:DDInternalLock=C:\\Windows\\System32\\ddraw.DDInternalLock,@5")
+#pragma comment(linker, "/export:DDInternalUnlock=C:\\Windows\\System32\\ddraw.DDInternalUnlock,@6")
+#pragma comment(linker, "/export:DSoundHelp=C:\\Windows\\System32\\ddraw.DSoundHelp,@7")
+
+#pragma comment(linker, "/export:DirectDrawCreate=_DirectDrawCreate@12,@8")
+#pragma comment(linker, "/export:DirectDrawCreateClipper=_DirectDrawCreateClipper@12,@9")
+#pragma comment(linker, "/export:DirectDrawCreateEx=_DirectDrawCreateEx@16,@10")
+#pragma comment(linker, "/export:DirectDrawEnumerateA=_DirectDrawEnumerateA@8,@11")
+#pragma comment(linker, "/export:DirectDrawEnumerateExA=_DirectDrawEnumerateExA@12,@12")
+#pragma comment(linker, "/export:DirectDrawEnumerateExW=_DirectDrawEnumerateExW@12,@13")
+#pragma comment(linker, "/export:DirectDrawEnumerateW=_DirectDrawEnumerateW@8,@14")
+
+#pragma comment(linker, "/export:GetDDSurfaceLocal=C:\\Windows\\System32\\ddraw.GetDDSurfaceLocal,@17")
+#pragma comment(linker, "/export:GetOLEThunkData=C:\\Windows\\System32\\ddraw.GetOLEThunkData,@18")
+#pragma comment(linker, "/export:GetSurfaceFromDC=C:\\Windows\\System32\\ddraw.GetSurfaceFromDC,@19")
+#pragma comment(linker, "/export:RegisterSpecialCase=C:\\Windows\\System32\\ddraw.RegisterSpecialCase,@20")
+#pragma comment(linker, "/export:ReleaseDDThreadLock=C:\\Windows\\System32\\ddraw.ReleaseDDThreadLock,@21")
+#pragma comment(linker, "/export:SetAppCompatData=C:\\Windows\\System32\\ddraw.SetAppCompatData,@22")
